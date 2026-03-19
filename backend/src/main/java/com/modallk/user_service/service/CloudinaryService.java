06411b1ebe0ff2d57b0fa4e32e9a2deaaf467459
kaveeshala <kaveeshala2003@gmail.com>
2026-03-20 00:20:38 +0530

    feat: add cloudinaryConfig and service files

---
+++ backend/src/main/java/com/modallk/user_service/service/CloudinaryService.java
@@ -0,0 +1,32 @@
+package com.modallk.user_service.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class CloudinaryService {
+
+    private final Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file, String folder) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.asMap(
+                        "folder", folder,
+                        "resource_type", "image"
+                )
+        );
+        return (String) uploadResult.get("secure_url");
+    }
+
+    public void deleteImage(String publicId) throws IOException {
+        cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
+    }
+}
